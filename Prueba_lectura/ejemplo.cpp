9337526d3f3b2ce53c113fcf2f413b8e196899bd
FPomi <77468266+FPomi@users.noreply.github.com>
2024-07-13 21:00:11 -0300

    Add files via upload

---
+++ Prueba_lectura/ejemplo.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include "lectura.h"
+#include "escritura.h"
+
+int main() {
+
+	std::string archivoConNotas = "datos/entrada.txt";
+	std::string archivoConSecuencia = "datos/salida.txt";
+
+	leerNotas(archivoConNotas);
+
+	escribirSecuencia(archivoConSecuencia);
+
+	return 0;
+}
---
+++ Prueba_lectura/ejemplo.h
@@ -0,0 +1,8 @@
+#ifndef EJEMPLO_H_
+#define EJEMPLO_H_
+
+#include <string>
+
+void escribirSecuencia(std::string rutaSalida);
+
+#endif // EJEMPLO_H_
